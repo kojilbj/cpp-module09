3caf84a507f279e290ad2344200cddf9414499fc
kojwatan <20020427ka@gmail.com>
2025-02-03 15:08:37 +0900

    07/ex00 completed

---
+++ 07/ex00/whatever.hpp
@@ -0,0 +1,14 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <class T>
+void	swap(T &a, T &b);
+
+template <class T>
+T	max(const T &a, const T &b);
+
+template <class T>
+T	min(const T &a, const T &b);
+
+#endif
+
