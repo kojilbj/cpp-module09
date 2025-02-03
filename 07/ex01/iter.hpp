370d4547dfe315249f842c2c58fedaed65607e18
kojwatan <20020427ka@gmail.com>
2025-02-03 23:24:20 +0900

    07/ex02 implement function template in header file

--- 07/ex01/iter.hpp
+++ 07/ex01/iter.hpp
@@ -1,2 +1,13 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
 template <class T>
-void	iter(T *arrayPtr, size_t length, void (*func)(T &));
+void	iter(T *arrayPtr, size_t length, void (*func)(T &))
+{
+	for (size_t i = 0; i < length; i++)
+	{
+		func(arrayPtr[i]);
+	}
+}
+
+#endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -4,15 +4,6 @@
 #include "iter.hpp"
 
 template <class T>
-void	iter(T *arrayPtr, size_t length, void (*func)(T &))
-{
-	for (size_t i = 0; i < length; i++)
-	{
-		func(arrayPtr[i]);
-	}
-}
-
-template <class T>
 void	print(T a)
 {
 	std::cout << a;
