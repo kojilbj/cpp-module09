156c3d639f9f61931e24bfdd641d537850d2b216
kojwatan <20020427ka@gmail.com>
2025-04-07 13:26:49 +0900

    add test case

--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -4,26 +4,53 @@
 
 int	main(void)
 {
-	std::vector<int> container;
-
-	container.push_back(1);
-	container.push_back(2);
-	container.push_back(3);
-
-	try
 	{
+		std::vector<int> container;
+
+		container.push_back(1);
+		container.push_back(2);
+		container.push_back(3);
+
+		std::cout << "vector: ";
+		for (std::vector<int>::iterator it = container.begin(); it != container.end(); it++)
+			std::cout << *it << ", ";
+		std::cout << std::endl;
+		// Test for Head Element
+		std::cout << *(::easyfind(container, 1)) << std::endl;
+
+		// Test for Middle Element
 		std::cout << *(::easyfind(container, 2)) << std::endl;
+
+		// Test for Last Element
+		std::cout << *(::easyfind(container, 3)) << std::endl;
+
+		// Test for Uncontains Element
+		if (::easyfind(container, 9) == container.end())
+			std::cout << "Not found" << std::endl;
 	}
-	catch (const std::exception &e)
+
 	{
-		std::cout << e.what() << std::endl;
-	}
-	try
-	{
-		std::cout << *(::easyfind(container, 9)) << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
+		std::deque<int> container;
+
+		container.push_back(1);
+		container.push_back(2);
+		container.push_back(3);
+
+		std::cout << "deque: ";
+		for (std::deque<int>::iterator it = container.begin(); it != container.end(); it++)
+			std::cout << *it << ", ";
+		std::cout << std::endl;
+		// Test for Head Element
+		std::cout << *(::easyfind(container, 1)) << std::endl;
+
+		// Test for Middle Element
+		std::cout << *(::easyfind(container, 2)) << std::endl;
+
+		// Test for Last Element
+		std::cout << *(::easyfind(container, 3)) << std::endl;
+
+		// Test for Uncontains Element
+		if (::easyfind(container, 9) == container.end())
+			std::cout << "Not found" << std::endl;
 	}
 }
