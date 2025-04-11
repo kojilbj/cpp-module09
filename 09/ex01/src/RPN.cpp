572eba7ce2312637c6df8433f1550f112afba7e6
kojwatan <20020427ka@gmail.com>
2025-04-11 10:26:09 +0900

    RPN logic change to switch

--- 09/ex01/src/RPN.cpp
+++ 09/ex01/src/RPN.cpp
@@ -4,6 +4,15 @@
 
 namespace RPN
 {
+	enum Operator
+	{
+		NONE,
+		ADD,
+		SUB,
+		MUL,
+		DIV
+	};
+
 	int toInt(const std::string& str)
 	{
 		std::stringstream ss(str);
@@ -12,7 +21,7 @@ namespace RPN
 
 		if (ss.fail())
 		{
-			throw std::runtime_error("Error");
+			throw std::runtime_error("Error toInt: " + str);
 		}
 
 		return result;
@@ -21,11 +30,24 @@ namespace RPN
 	void getValues(std::stack<int>& stack, int& left, int& right)
 	{
 		if (stack.size() < 2)
-			throw std::runtime_error("Error");
+			throw std::runtime_error("Error getValues");
 		right = stack.top();
 		stack.pop();
 		left = stack.top();
 		stack.pop();
+	}
+
+	Operator getOperator(const std::string& op)
+	{
+		if (op == "+")
+			return ADD;
+		if (op == "-")
+			return SUB;
+		if (op == "/")
+			return DIV;
+		if (op == "*")
+			return MUL;
+		return NONE;
 	}
 
 	int calculate(std::stringstream fomula)
@@ -35,26 +57,29 @@ namespace RPN
 
 		while (std::getline(fomula, buff, ' '))
 		{
+			std::cout << buff << std::endl;
 			int right, left;
-			if (buff == "+")
+			Operator op = getOperator(buff);
+			if (op != NONE)
 			{
 				getValues(stack, left, right);
-				stack.push(left + right);
-			}
-			else if (buff == "-")
-			{
-				getValues(stack, left, right);
-				stack.push(left - right);
-			}
-			else if (buff == "*")
-			{
-				getValues(stack, left, right);
-				stack.push(left * right);
-			}
-			else if (buff == "/")
-			{
-				getValues(stack, left, right);
-				stack.push(left / right);
+				switch (op)
+				{
+				case ADD:
+					stack.push(left + right);
+					break;
+				case SUB:
+					stack.push(left - right);
+					break;
+				case MUL:
+					stack.push(left * right);
+					break;
+				case DIV:
+					stack.push(left / right);
+					break;
+				default:
+					break;
+				}
 			}
 			else
 			{
@@ -62,7 +87,7 @@ namespace RPN
 			}
 		}
 		if (stack.size() != 1)
-			throw std::runtime_error("Error");
+			throw std::runtime_error("Error calcurate");
 		return stack.top();
 	}
 
